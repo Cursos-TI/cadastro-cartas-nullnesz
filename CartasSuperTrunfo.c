8e60179ee33e80d4c653b625c562ff85e09bf753
nullnesz <thsnullnes@gmail.com>
2025-11-11 22:46:00 +0000

    Adicionado alguns cálculos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,8 @@ int main() {
     float area1;
     float pib1;
     int pontos1;
-
+    float densidade1;
+    float pibPerCapita1;
 
     //carta 2
     char estado2;
@@ -19,6 +20,8 @@ int main() {
     float area2;
     float pib2;
     int pontos2;
+    float densidade2;
+    float pibPerCapita2;
 
     printf("\n Cadastro da Carta 1 \n");
     printf("Estado (A-H): ");
@@ -42,6 +45,10 @@ int main() {
     printf("Numero de Pontos Turisticos: ");
     scanf(" %d", &pontos1);
 
+    //  CÁLCULOS CARTA 1 
+    densidade1 = populacao1 / area1;
+    pibPerCapita1 = (pib1 * 1000000000.0) / populacao1;
+
     //------------------------------------
     printf("\nCarta 1 \n");
     printf("Estado: %c\n", estado1);
@@ -51,10 +58,11 @@ int main() {
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhoes de reais\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n", pontos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
     //-------------------------------------
 
-
-    printf("\n=== Cadastro da Carta 2 ===\n");
+    printf("\n Cadastro da Carta 2 \n");
     printf("Estado (A-H): ");
     scanf(" %c", &estado2);
 
@@ -76,6 +84,10 @@ int main() {
     printf("Numero de Pontos Turisticos: ");
     scanf(" %d", &pontos2);
 
+    // CÁLCULOS CARTA 2 
+    densidade2 = populacao2 / area2;
+    pibPerCapita2 = (pib2 * 1000000000.0) / populacao2;
+
     //-------------------------------------
     printf("\n Carta 2  \n");
     printf("Estado: %c\n", estado2);
@@ -85,6 +97,8 @@ int main() {
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhoes de reais\n", pib2);
     printf("Numero de Pontos Turisticos: %d\n", pontos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
     //-------------------------------------
 
     return 0;
